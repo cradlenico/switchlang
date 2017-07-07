e71a67a5d94d55aaa6f5a1105e11205f792cc789
cradlenico <nicolas75.geay@gmail.com>
2017-07-07 09:48:23 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,28 +1,16 @@
 <?php
+use \App\Autoloader;
 
+require 'app/class/Autoloader.php';
+Autoloader::register();
+$lang = new App\Lang();
+$json = $lang->expose();
+require 'app/inc/_header.php';
+require 'app/inc/_nav.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <div class="container">
+        <h1><?= $json->homeTitle; ?></h1>
+    </div>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-</head>
-<body>
-
-<?php require 'app/inc/_nav.php'; ?>
-
-<div class="container">
-    <h1>Hello, world!</h1>
-</div>
-
-<!-- jQuery first, then Tether, then Bootstrap JS. -->
-<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-</body>
-</html>
+<?php require 'app/inc/_footer.php'; ?>
