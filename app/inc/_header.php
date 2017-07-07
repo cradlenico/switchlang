07434972b1a069809e7242fd2f2c69815acf9fd6
cradlenico <nicolas75.geay@gmail.com>
2017-07-07 09:53:59 +0200

    Add files via upload

--- app/inc/_header.php
+++ app/inc/_header.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
 <html lang="<?= $json->lang; ?>">
 <head>
-    <title><?= $json->home;?> | PHP-i18n</title>
+    <title><?= $json->home; ?> | PHP-i18n</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
+          integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <style>
+
+        .starter-template {
+            padding: 3rem 1.5rem;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
 
