5cf7ab7e4062d10b34a6de18fccfb76f65a93b6b
cradlenico <nicolas75.geay@gmail.com>
2017-07-07 10:30:19 +0200

    Add files via upload

--- app/class/Lang.php
+++ app/class/Lang.php
@@ -12,10 +12,14 @@ class Lang
     public function expose()
     {
         $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-        if ($lang != 'fr') :
-            $content = file_get_contents("app/i18n/en.json", true);
-        else :
+        if ($lang == 'fr') :
             $content = file_get_contents("app/i18n/$lang.json", true);
+        elseif ($lang == 'zh') :
+            //chinese language
+            $content = file_get_contents("app/i18n/$lang.json", true);
+        else :
+            //default language
+            $content = file_get_contents("app/i18n/en.json", true);
         endif;
         return $json = json_decode($content);
     }
