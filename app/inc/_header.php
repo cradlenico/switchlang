6c6216469846f9abd2e02b210b81b9a603ff5bb7
cradlenico <nicolas75.geay@gmail.com>
2017-07-07 09:47:44 +0200

    Add files via upload

---
+++ app/inc/_header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="<?= $json->lang; ?>">
+<head>
+    <title><?= $json->home;?> | PHP-i18n</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+</head>
+<body>
